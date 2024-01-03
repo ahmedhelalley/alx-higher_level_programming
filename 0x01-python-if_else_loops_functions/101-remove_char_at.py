e02401fa1ccb8098a4e9ca8faa9a0c31ff7aec0b
ahmedhelalley <a.helalley@gmail.com>
2024-01-03 03:57:29 +0200

    msg

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 def remove_char_at(str, n):
+    newstr = ""
     for i in range(len(str)):
         if i == n:
             continue
         else:
-            return "{}".format(str[i])
+            newstr = newstr + str[i]
+    return newstr
