fe299f956c7c3bbc611f92f05a3da454c0ca2bd4
ahmedhelalley <a.helalley@gmail.com>
2024-01-06 16:29:22 +0200

    msg

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -2,8 +2,6 @@
 def no_c(my_string):
     ret = ""
     for i in range(len(my_string)):
-        if 'c' in my_string or 'C' in my_string:
-            continue
-        else:
+        if (my_string[i] != 'c' and my_string[i] != 'C'):
             ret += my_string[i]
-        return ret
+    return ret
