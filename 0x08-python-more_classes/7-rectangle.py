030e176b100825a1a46711ac9a66f8bc20945173
ahmedhelalley <a.helalley@gmail.com>
2024-01-29 18:34:13 +0200

    msg

--- 0x08-python-more_classes/7-rectangle.py
+++ 0x08-python-more_classes/7-rectangle.py
@@ -63,7 +63,7 @@ class Rectangle:
         """returns a printable representation of the rec"""
         string = ""
         if self.__width != 0 and self.__height != 0:
-            string += "\n".join("#" * self.__width
+            string += "\n".join(str(self.print_symbol) * self.__width
                                 for j in range(self.__height))
         return string
 
