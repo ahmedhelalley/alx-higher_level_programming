ed6e3352f504735d7bc2e7e97a5862bd3e6a796d
ahmedhelalley <a.helalley@gmail.com>
2024-02-07 03:33:36 +0200

    msg

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""Class student"""
+
+
+class Student:
+    """student representation"""
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """retrieving student dict"""
+        try:
+            for attr in attrs:
+                if type(attr) is not str:
+                    return self.__dict__
+        except Exception:
+            return self.__dict__
+        my_dict = {}
+        for key, value in self.__dict__.items():
+            if key in attrs:
+                my_dict[key] = value
+        return my_dict
+
+    def reload_from_json(self, json):
+        """replacing instances in student"""
+        for key, value in json.items():
+            if key in self.__dict__:
+                self.__dict__[key] = [value]
