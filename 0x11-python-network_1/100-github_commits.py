fc10fda45a285258f83f81aa936dc947034f99f7
ahmedhelalley <a.helalley@gmail.com>
2024-04-29 21:54:44 +0300

    msg

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""
+    Lists the latest of a specific repository by a specific user,
+    using the GitHub API
+"""
+
+import sys
+import requests
+
+if __name__ == "__main__":
+    owner = sys.argv[2]
+    repo_name = sys.argv[1]
+    url = f"https://api.github.com/repos/{owner}/{repo_name}/commits"
+
+    response = requests.get(url).json()
+
+    commits_list = []
+    for commit in response:
+
+        commit_dict = {
+            "sha": commit.get("sha"),
+            "author": commit.get("commit").get("author").get("name"),
+            "time": commit.get("commit").get("author").get("date")
+        }
+        commits_list.append(commit_dict)
+
+    sorted_commits = sorted(commits_list,
+                            key=lambda x: x['time'],
+                            reverse=True)
+
+    for idx in range(min(10, len(sorted_commits))):
+        sha = sorted_commits[idx].get("sha")
+        author = sorted_commits[idx].get("author")
+        print("{}: {}".format(sha, author))
